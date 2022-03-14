526b3e934ec322e07a81c1604e3193e49eb871a7
Ferdinand Malcher <ferdinand@malcher.media>
2022-03-14 12:23:18 +0100

    Interface Book und Buchdaten

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-dashboard',
@@ -7,9 +8,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  books: Book[] = [];
+
+  constructor() {
+    this.books = [
+      {
+        isbn: '123',
+        title: 'Angular',
+        description: 'Grundlagen und mehr',
+        price: 36.9,
+        rating: 5
+      },
+      {
+        isbn: '456',
+        title: 'Vue.js',
+        description: 'Das andere Framework',
+        price: 32.9,
+        rating: 3
+      }
+    ]
+  }
 
   ngOnInit(): void {
   }
 
 }
+
+/*
+// Klassen vs. Interfaces
+
+class BookC {
+  rating = 3
+  constructor(public isbn: string, public title: string) {}
+
+  rateUp() {
+    this.rating++;
+  }
+}
+
+const myBook = new BookC('123', 'Angular');
+
+
+///////////////
+
+
+interface BookI {
+  isbn: string;
+  title: string;
+}
+
+const myBook2: BookI = {
+  isbn: '1234',
+  title: ''
+};
+
+
+
+// JSON
+{
+  "title": "Angular",
+  "isbn": "123"
+}
+
+
+*/
