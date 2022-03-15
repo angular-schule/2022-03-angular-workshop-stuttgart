ba8f90c2fe1f50dc98d47e4669ba83a0511827dd
Ferdinand Malcher <ferdinand@malcher.media>
2022-03-15 16:08:49 +0100

    Routing: Routen und Links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  { path: 'books', component: DashboardComponent },
+  { path: 'books/details/:isbn', component: BookDetailsComponent }, // books/details/467895398
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+/*
+TODO:
+- Weiterleitung ☑️
+- Links
+- Detailseite
+
+
+
+- (Create / Formular)
+
+
+
+
+
+
+*/
