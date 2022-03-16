368768c7989d2b72af2329ca4e9c12ca4d883a7b
Ferdinand Malcher <ferdinand@malcher.media>
2022-03-16 15:13:35 +0100

    Fun with Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'br-root',
@@ -7,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Book Rating';
+
+  constructor() {
+
+    function producer(subscriber: any) {
+      subscriber.next('HALLO!');
+      subscriber.next(Math.random());
+      subscriber.next(5);
+      subscriber.next(7);
+
+      setTimeout(() => {
+        subscriber.error('BÖSER FEHLER!');
+      }, 2000);
+    }
+
+
+    const observer = {
+      next: (data: any) => {
+        console.log(data);
+      },
+      error: (err: any) => {
+        console.log('ERROR', err);
+      },
+      complete: () => {
+        console.log('Complete');
+      }
+    };
+
+    // producer(observer);
+
+    const myObservable$ = new Observable(producer);
+    // myObservable$.subscribe(observer);
+
+    // myObservable$.subscribe(value => console.log('VALUE!', value));
+
+  }
 }
