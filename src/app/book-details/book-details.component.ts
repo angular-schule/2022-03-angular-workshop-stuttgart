07c054d1ccc109d61493514ee52a512fd4b0251c
Ferdinand Malcher <ferdinand@malcher.media>
2022-03-16 11:03:15 +0100

    Routing: Detailseite

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-book-details',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookDetailsComponent implements OnInit {
 
-  constructor() { }
+  book?: Book;
+
+  constructor(
+    private route: ActivatedRoute,
+    private service: BookStoreService
+  ) {
+    const isbn = this.route.snapshot.paramMap.get('isbn'); // /books/details/:isbn
+    if (isbn) {
+      console.log(isbn);
+
+      this.service.getSingle(isbn).subscribe(book => {
+        this.book = book;
+      });
+    }
+  }
 
   ngOnInit(): void {
   }
 
 }
+
+
+/*
+TODO:
+  - ISBN aus der URL auslesen
+  - BookStoreService.getSingle()
+  - Anzeige
+*/
